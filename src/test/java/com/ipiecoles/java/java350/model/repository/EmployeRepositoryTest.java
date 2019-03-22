b8e43c8a8819a37f5ffcdde3857e3db3aed1e242
mduf <maxime.dufrasne@free.fr>
2019-03-22 10:06:48 +0100

    test travis

---
+++ src/test/java/com/ipiecoles/java/java350/model/repository/EmployeRepositoryTest.java
@@ -0,0 +1,62 @@
+import com.ipiecoles.java.java350.model.Employe;
+import com.ipiecoles.java.java350.model.Entreprise;
+import com.ipiecoles.java.java350.repository.EmployeRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+public class EmployeRepositoryTest {
+
+
+    @Autowired
+    private EmployeRepository employeRepository;
+
+    @BeforeEach
+    @AfterEach
+    public void setup(){
+        employeRepository.deleteAll();
+    }
+
+    @Test
+    public void testFindLastMatricule(){
+        //Given
+
+        //When
+        String lastMatricule = employeRepository.findLastMatricule();
+        //Then
+        Assertions.assertEquals(null, lastMatricule);
+    }
+
+    @Test
+    public void testFindLastMatriculeSingle(){
+        //Given
+        Employe e = new Employe("Doe", "John", "T12345", LocalDate.now(), Entreprise.SALAIRE_BASE, 1, 1.0);
+        employeRepository.save(e);
+        //When
+        String lastMatricule = employeRepository.findLastMatricule();
+        //Then
+        Assertions.assertEquals("12345", lastMatricule);
+    }
+
+    @Test
+    public void testFindLastMatriculeMultiple(){
+        //Given
+        employeRepository.save(new Employe("Doe", "John", "T12345", LocalDate.now(), Entreprise.SALAIRE_BASE, 1, 1.0));
+        employeRepository.save(new Employe("Doe", "Jane", "M40325", LocalDate.now(), Entreprise.SALAIRE_BASE, 1, 1.0));
+        employeRepository.save(new Employe("Doe", "Jim", "C06432", LocalDate.now(), Entreprise.SALAIRE_BASE, 1, 1.0));
+
+        //When
+        String lastMatricule = employeRepository.findLastMatricule();
+
+        //Then
+        Assertions.assertEquals("40325", lastMatricule);
+    }
+
+
+}
